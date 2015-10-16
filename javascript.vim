75cc8b041cdaecc66f3bc026ad71e3b5f1351913
Tommy Heffernan <tdheff@gmail.com>
2015-10-16 17:47:25 -0400

    added whole vim directory

--- javascript.vim
+++
@@ -1,4 +0,0 @@
-" Code folding
-syntax region foldBraces start=/{/ end=/}/ transparent fold keepend extend
-setlocal foldmethod=syntax
-setlocal foldlevel=99
--- setup.sh
+++ setup.sh
@@ -1,15 +1,8 @@
 # download and install vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# setup javascript code folding
-mkdir ~/.vim/ftplugin/
-cp ./javascript.vim ~/.vim/ftplugin/javascript.vim
-
-# get terminal colors
-git clone git@github.com:tdheff/base16-melon ~/dev/base16-melon
-
 # copy vimrc
 cp ./vimrc ~/.vimrc
 
-# copy fish config
-cp ./config.fish ~/.config/fish/config.fish
+# copy vim directory
+cp -r ./vim ~/.vim
